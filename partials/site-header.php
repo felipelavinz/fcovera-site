dbd6b92e6b5be5995f59288b4ee74c2d8bc191d7
Felipe Lavín Z <felipe@bloom-ux.com>
2019-03-14 14:51:14 -0300

    mejoras de estilos y traducciones

--- partials/site-header.php
+++ partials/site-header.php
@@ -5,9 +5,9 @@
 				<a href="<?php echo function_exists('pll_home_url') ? pll_home_url() : site_url(); ?>">Francisco Vera</a>
 			</h1>
 			<nav>
-				<a class="--writings" href="<?php echo esc_attr_x('/writings/', 'url seccion', 'chopan_2019'); ?>"><?php echo esc_html_x( 'Writings', 'header', 'chopan_2019' ); ?></a>
-				<a class="--work" href="<?php echo esc_attr_x('/work/', 'url seccion', 'chopan_2019'); ?>"><?php echo esc_html_x( 'Work', 'header', 'chopan_2019' ); ?></a>
-				<a class="--me" href="<?php echo esc_attr_x('/me/', 'url seccion', 'chopan_2019'); ?>"><?php echo esc_html_x( 'Me', 'header', 'chopan_2019' ); ?></a>
+				<a class="--writings <?php echo function_exists('pll_current_language') ? pll_current_language('slug') : ''; ?>" href="<?php echo esc_attr_x('/writings/', 'url seccion', 'chopan_2019'); ?>"><?php echo esc_html_x( 'Writings', 'header', 'chopan_2019' ); ?></a>
+				<a class="--work <?php echo function_exists('pll_current_language') ? pll_current_language('slug') : ''; ?>" href="<?php echo esc_attr_x('/work/', 'url seccion', 'chopan_2019'); ?>"><?php echo esc_html_x( 'Work', 'header', 'chopan_2019' ); ?></a>
+				<a class="--me <?php echo function_exists('pll_current_language') ? pll_current_language('slug') : ''; ?>" href="<?php echo esc_attr_x('/me/', 'url seccion', 'chopan_2019'); ?>"><?php echo esc_html_x( 'Me', 'header', 'chopan_2019' ); ?></a>
 			</nav>
 		</div>
 		<div class="-right">
